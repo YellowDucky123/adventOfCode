b00ff9a9f4c74baafa3cbcd13ec5760c27282acf
Kelvin Jonathan Yoga <kelvinjyoga@gmail.com>
2024-12-23 19:51:14 +1100

    fix

--- Day18/day18P2.cpp
+++ Day18/day18P2.cpp
@@ -7,18 +7,11 @@
 
 using namespace std;
 
-class myClass {
-    public:
-        int x;
-        int y;
-};
-
 void byteFallSimulation(char** grid, ifstream& fp);
 int* earliestCoord(char** grid, ifstream& fp);
 bool bfs(char** grid);
 void byteFallSimulation(char** grid, ifstream& fp);
-int dijktras(char** grid);
-void minDistance(int distance[71][71], bool sptV[71][71], int v[2]);
+
 
 int main() {
     ifstream fp("input.txt");
@@ -58,7 +51,7 @@ int main() {
 
 int* earliestCoord(char** grid, ifstream& fp) {
     int x, y;
-    while(dijktras(grid) != INT16_MAX) {
+    while(bfs(grid)) {
         for(int i = 0; i < 71; i++) {
             for(int j = 0; j < 71; j++) {
                 cout << grid[i][j];
@@ -79,54 +72,6 @@ int* earliestCoord(char** grid, ifstream& fp) {
     return ret;
 }
 
-
-int dijktras(char** grid) {
-    int distance[71][71];
-    bool sptV[71][71];
-    for(int i = 0; i < 71; i++) {
-        for(int j = 0; j < 71; j++) {
-            distance[i][j] = INT16_MAX;
-            sptV[i][j] = false;
-        }
-    }
-
-    distance[0][0] = 0;
-
-    for(int i = 0; i < 71; i++) {
-        for(int j = 0; j < 71; j++) {
-            int v[2];
-            minDistance(distance, sptV, v);
-            sptV[v[1]][v[0]] = true;
-
-            int direction[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-            for(int i = 0; i < 4; i++) {
-                int newX = v[0] + direction[i][0];
-                int newY = v[1] + direction[i][1];
-                if(newX > 70 || newX < 0 || newY > 70 || newY < 0 || grid[newY][newX] == '#') continue;
-                
-                if(distance[newY][newX] > distance[v[1]][v[0]] + 1) {
-                    distance[newY][newX] = distance[v[1]][v[0]] + 1;
-                }
-            }
-        }
-    }
-
-    return distance[70][70];
-}
-
-void minDistance(int distance[71][71], bool sptV[71][71], int v[2]) {
-    int min = INT16_MAX;
-    for(int i = 0; i < 71; i++) {
-        for(int j = 0; j < 71; j++) {
-            if(distance[i][j] < min && !sptV[i][j]) {
-                min = distance[i][j];
-                v[0] = j;
-                v[1] = i;
-            }
-        }
-    }
-}
-
 bool bfs(char** grid) {
     bool** visited = new bool*[71];
     for(int i = 0; i < 71; i++) {
@@ -139,31 +84,27 @@ bool bfs(char** grid) {
         }
     }
 
-    queue<myClass> q;
-    myClass v;
-    v.x = 0;
-    v.y = 0;
-    q.push(v);
+    queue<pair<int, int>> q;
+    q.push({0, 0});
     visited[0][0] = true;
 
     while(!q.empty()) {
-        myClass ver = q.front();
+        pair<int, int> ver = q.front();
         q.pop();
+        int verX = ver.first, verY = ver.second;
 
-        if(ver.x == 70 && ver.y == 70) {
+        if(verX == 70 && verY == 70) {
             return true;
         }
 
         int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         for(int i = 0; i < 4; i++) {
-            int newX = ver.x + directions[i][0];
-            int newY = ver.y + directions[i][1];
+            int newX = verX + directions[i][0];
+            int newY = verY + directions[i][1];
             if(newX > 70 || newX < 0 || newY > 70 || newY < 0 || grid[newY][newX] == '#' || visited[newY][newX]) continue;
 
-            myClass tmp;
-            tmp.x = newX;
-            tmp.y = newY;
-            q.push(tmp);
+            q.push({newX, newY});
+            visited[newY][newX] = true;
         }
     }
 
