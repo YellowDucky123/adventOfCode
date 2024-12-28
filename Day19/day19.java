019b9e5b9d5de19be05549899f13d8bab037f22d
Kelvin Jonathan Yoga <kelvinjyoga@gmail.com>
2024-12-28 19:44:12 +1100

    day19 finish

--- Day19/day19.java
+++ Day19/day19.java
@@ -1,71 +1,75 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
-import java.util.Vector; // Import the Vector class
-import java.util.StringTokenizer; // Import the StringTokenizer class
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.StringTokenizer;
 import java.util.HashMap;
 import java.util.Map;
+import java.math.BigInteger;
 
 public class day19 {
-    private static Map<String, Integer> cache = new HashMap<>();
+    private static Map<String, BigInteger> cache = new HashMap<>();
 
     public static void main(String[] args) {
         // Initialize the file and vector
         File myObj = new File("input.txt");
 
         Vector<String> words = new Vector<>();
-        Vector<String> str = new Vector <>(); //to be validated
+        Vector<String> str = new Vector<>(); //to be validated
 
         // Read the file and populate the vector
         readFile(myObj, words, str);
 
         int count = 0;
-
-        for(String s: str) {
+        BigInteger overall = BigInteger.ZERO;
+        for (String s : str) {
             System.out.println(s);
-            if(valid(s, words) > 0) {
+            BigInteger tmp = valid(s, words);
+            if (tmp.compareTo(BigInteger.ZERO) > 0) {
                 System.out.println("isValid\n");
                 count++;
-            }
-            else {
+                overall = overall.add(tmp);
+            } else {
                 System.out.println("notValid-----------\n");
             }
         }
 
         System.out.println("part 1 count is " + count + "\n");
+        System.out.println("part 2 count is " + overall + "\n");
     }
 
-    public static int valid(String design, Vector<String> pattern) {
-        if(design.isEmpty()) {
-            return 1;
+    public static BigInteger valid(String design, Vector<String> pattern) {
+        if (design.isEmpty()) {
+            return BigInteger.ONE;
         }
-
-        if(cache.containsKey(design)) {
+        design = design.trim();
+        // System.out.println(design);
+        if (cache.containsKey(design)) {
+            System.out.println("here\n");
+            System.out.println("get is " + cache.get(design));
             return cache.get(design);
         }
 
-        int design_ways = 0;
-        for(String pat : pattern) {
-            if(startsWith(design, pat)) {
-                int res = valid(design.substring(pat.length()), pattern);
-                design_ways += res;
+        BigInteger design_ways = BigInteger.ZERO;
+        for (String pat : pattern) {
+            if (startsWith(design, pat)) {
+                BigInteger res = valid(design.substring(pat.length()), pattern);
+                System.out.println("res is " + res);
+                design_ways = design_ways.add(res);
             }
         }
-
+        System.out.println("designways " + design_ways);
         cache.put(design, design_ways);
         return design_ways;
     }
 
     static boolean startsWith(String design, String pattern) {
         int length = pattern.length();
-        if(design.length() < length) {
+        if (design.length() < length) {
             return false;
         }
         String sub = design.substring(0, length);
-        if(sub.equals(pattern)) {
-            return true;
-        }
-        return false;
+        return sub.equals(pattern);
     }
 
     static void readFile(File myObj, Vector<String> v, Vector<String> str) {
@@ -85,11 +89,10 @@ public class day19 {
             // Read each line and tokenize it
             while (myReader.hasNextLine()) {
                 String sen = myReader.nextLine();
-                str.add(sen); 
+                str.add(sen);
             }
             myReader.close();
-        } 
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
---
+++ Day19/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        String str = "testing12345";
+        String pat = "test";
+        System.out.println(str.substring(pat.length()));
+    }
+}
